7f8c1cede81cb629f6ea8a384f7482174256f551
Mimi Esposito <mikisole122@gmail.com>
2022-05-24 17:07:38 -0300

    .

---
+++ Mimi/Loguin/bdlogueo/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class UsariosBO(models.Model):
+    usuario=models.CharField(max_length=40)
+    contraseña=models.CharField(max_length=40)
+    oblea=models.CharField(max_length=40)
+    legajo=models.CharField(max_length=7)
+
---
+++ Mimi/Loguin/bdlogueo/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from bdlogueo.models import UsariosBO
+
+# Create your views here.
+def loguin(request):
